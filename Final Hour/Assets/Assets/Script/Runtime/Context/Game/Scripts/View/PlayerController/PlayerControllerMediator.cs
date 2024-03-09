c84ae805ff6a615c72a40d13627dcc737029dbac
Engin Can Altınöz <altinozengincan@gmail.com>
2024-03-09 17:01:40 +0300

    ADD: coruch and jumping added.

---
+++ Final Hour/Assets/Assets/Script/Runtime/Context/Game/Scripts/View/PlayerController/PlayerControllerMediator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using strange.extensions.mediation.impl;
+using UnityEngine;
+
+public class PlayerControllerMediator : EventMediator
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void deneme()
+    {
+
+
+
+
+    }
+}
